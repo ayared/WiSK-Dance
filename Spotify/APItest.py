a684321585d99167352e6e54929232c8cf294926
Alexander Yared <akyared@gmail.com>
2018-05-13 00:28:48 -0400

    Autoplay a song searched for by the user

--- Spotify/APItest.py
+++ Spotify/APItest.py
@@ -1,26 +1,21 @@
 # Test environment for querying Spotify API
 import spotipy
-import spotipy.util as util
 import ReadToken
-
-scope = 'user-library-read'
-username = 'WiSK-Dance'
-
-SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI = ReadToken.ReadToken('SpotifyToken.txt')
-# Redirect URL given by Spotify after successful execution of this code:
-# https://example.com/callback/?code=AQCQaOt_4YgiUcK1i7ijXXo1-leHsG8TMnAo1w-9loBT0d52jDprYN1kNp7XJ4f69nJK4PpGCSV0oO1GEg3DjUcm8ipBaURFMyd3ZA2Y4-epTNf9duw1MlffitFV41mamn-6JEKdHLwl069z9ly__wb3rYoNFv-QLQZJi6ibgGO9zYzxn7StU3dpT86Dr0T4gyYcdcy5yK6l2CViI11qlt_bIQ7_Fg
-token = util.prompt_for_user_token(
-    username,
-    scope,
-    client_id=SPOTIPY_CLIENT_ID,
-    client_secret=SPOTIPY_CLIENT_SECRET,
-    redirect_uri=SPOTIPY_REDIRECT_URI)
-
+import webbrowser
+token = ReadToken.GetToken('SpotifyToken.txt')
 spotify = spotipy.Spotify(auth=token)
 
 # Pulled from Spotify search
-j5_uri = 'spotify:artist:6wFId9Jhuf9AKVzWboOj2B' 
-results = spotify.artist_top_tracks(j5_uri)
+# j5_uri = 'spotify:artist:6wFId9Jhuf9AKVzWboOj2B' 
+# results = spotify.artist_top_tracks(j5_uri)
+prompt = input('Search for a song: ')
+results = spotify.search(prompt,type='track')
 
-for track in results['tracks'][:10]:
-    print('track: ' + track['name'])
+# for item in results['tracks']['items']:
+#      print('track: ' + item['name'])
+
+# print(results['tracks']['items'][2])
+
+url = results['tracks']['items'][2]['external_urls']['spotify']
+
+webbrowser.open(url)
--- Spotify/ReadToken.py
+++ Spotify/ReadToken.py
@@ -6,7 +6,9 @@
 #CLIENT_SECRET
 #REDIRECT_URI
 
-def ReadToken(fileName): 
+import spotipy.util as util
+
+def ReadFile(fileName): 
     f = open(fileName)
     tokens = f.read().splitlines()
     f.close()
@@ -16,7 +18,23 @@ def ReadToken(fileName):
     return CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
 def unitTest():
-    a, b, c = ReadToken('SpotifyToken.txt')
+    a, b, c = ReadFile('SpotifyToken.txt')
     print(a)
     print(b)
     print(c)
+    return
+
+def GetToken(fileName):
+    scope = 'user-library-read'
+    username = 'WiSK-Dance'
+    SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI = ReadFile(fileName)
+    token = util.prompt_for_user_token(
+        username,
+        scope,
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri=SPOTIPY_REDIRECT_URI)
+    return token
+
+# Redirect URL given by Spotify after successful execution of this code:
+# https://example.com/callback/?code=AQCQaOt_4YgiUcK1i7ijXXo1-leHsG8TMnAo1w-9loBT0d52jDprYN1kNp7XJ4f69nJK4PpGCSV0oO1GEg3DjUcm8ipBaURFMyd3ZA2Y4-epTNf9duw1MlffitFV41mamn-6JEKdHLwl069z9ly__wb3rYoNFv-QLQZJi6ibgGO9zYzxn7StU3dpT86Dr0T4gyYcdcy5yK6l2CViI11qlt_bIQ7_Fg
