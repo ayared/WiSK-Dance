17608bef64b1534fc332916b02218a1642289c31
JakeHellman <socketjake321@aol.com>
2018-05-27 13:19:07 -0400

    GUI code now displays sequence of arrows

--- visualizer.py
+++ visualizer.py
@@ -2,7 +2,7 @@
 # The GUI file controlling WiSK Dance's visualization
 
 try:
-    import sys, pygame, os
+    import sys, pygame, os, random
     from pygame.locals import *
     from enum import Enum
 except ImportError:
@@ -11,29 +11,39 @@ except ImportError:
 
 class Direction(Enum):
     LEFT = 0
-    UP = 1
-    RIGHT = 2
-    DOWN = 3
+    DOWN = 1
+    UP = 2
+    RIGHT = 3
 
 class Difficulty(Enum):
     EASY = 0
     MEDIUM = 1
     HARD = 2
 
-def load_png(name):
-    """ Load image and return image object"""
-    #fullname = os.path.join('data', name)
-    fullname = name
+SCREENRECT = Rect(0, 0, 1280, 720)
+SCORE = 0
+DIFF = Difficulty.EASY
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+def load_image(file):
+    "loads an image, prepares it for play"
+    file = os.path.join(main_dir, file)
     try:
-        image = pygame.image.load(fullname)
-        if image.get_alpha() is None:
-            image = image.convert()
+        surface = pygame.image.load(file)
+        if surface.get_alpha() is None:
+            surface.convert()
         else:
-            image = image.convert_alpha()
+            surface = surface.convert_alpha()
     except pygame.error:
-        print('Cannot load image:', fullname)
-        raise SystemExit
-    return image, image.get_rect()
+        raise SystemExit('Could not load image "%s" %s'%(file, pygame.get_error()))
+    return surface.convert()
+
+def load_images(*files):
+    imgs = []
+    for file in files:
+        imgs.append(load_image(file))
+    return imgs
 
 class Arrow(pygame.sprite.Sprite):
     """An arrow that will move across the screen
@@ -41,100 +51,97 @@ class Arrow(pygame.sprite.Sprite):
     Functions: update, rot_center, remove
     Attributes: image, rect, area, difficulty, direction, speed"""
 
-    def __init__(self, difficulty, direction, time, height):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_png('arrow.png')
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.difficulty = difficulty                            #0 = easy, 1 = medium, 2 = hard
-        self.direction = direction                              #0 = left, 1 = down, 2 = up, 3 = right
-        self.time = time
-        if self.direction == Direction.LEFT:
-            self.image = self.rot_center(90)
-            self.rect = self.rect.move(0,height)
-        elif self.direction == Direction.DOWN:
-            self.image = self.rot_center(180)
-            self.rect = self.rect.move(255,height)
-        elif self.direction == Direction.UP:
-            self.rect = self.rect.move(510,height)
-        elif self.direction == Direction.RIGHT:
-            self.image = self.rot_center(-90)
-            self.rect = self.rect.move(765,height)
-        if self.difficulty == Difficulty.EASY:
-            self.speed = -2
-        elif self.difficulty == Difficulty.MEDIUM:
-            self.speed = -4
-        elif self.difficulty == Difficulty.HARD:
-            self.speed = -6
+    images = []
+    def __init__(self, direction):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.direction = direction
+        self.speed = -2                 #FIX THIS TO SAY (DIFF+1)*(-2)
+        self.image = self.images[self.direction]
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(SCREENRECT.width*self.direction/4 + (SCREENRECT.width/4 - self.rect.width)/2, SCREENRECT.height)
+        self.frame = 0
 
     def update(self):
         """update the location of the arrow"""
-        self.rect = self.rect.move(0,self.speed)
-        if self.rect.top <= 0:
+        self.rect.move_ip(0, self.speed)
+        self.frame = self.frame + 1
+        if self.rect.bottom <= 0:
             self.kill()
 
-    def rot_center(self, angle):
-        """rotate an image while keeping its center and size"""
-        orig_rect = self.rect
-        rot_image = pygame.transform.rotate(self.image, angle)
-        rot_rect = orig_rect.copy()
-        rot_rect.center = rot_image.get_rect().center
-        rot_image = rot_image.subsurface(rot_rect).copy()
-        return rot_image
+def main(winstyle = 0):
+    # Initialize pygame
+    pygame.init()
 
-    def get_time(self):
-        return self.time
+    # Set the display mode
+    winstyle = 0  # fullscreen
+    bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
+    screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
 
-def main():
-    # Initialise screen
-    pygame.init()   
+    #Load images, assign to sprite classes
+    #(do this before the classes are used, after screen setup)
+    img = load_image('arrow.png')
+    Arrow.images = [pygame.transform.rotate(img, 90), pygame.transform.rotate(img, 180), img, pygame.transform.rotate(img, -90)]
+    
+    #decorate the game window
+    icon = pygame.transform.scale(load_image('WiSK_Logo.png'), (128, 128))
+    pygame.display.set_icon(icon)
     pygame.display.set_caption('WiSK Dance')
-    size = width, height = 1020, 600
-    black = 0, 0, 0
-    screen = pygame.display.set_mode(size)
+    pygame.mouse.set_visible(0)
 
-    # Fill background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill(black)
-
-    # Set difficulty
-    difficulty = Difficulty.EASY                                       #0 = easy, 1 = medium, 2 = hard
-
-    # Initialize arrows
-    #uparrow = Arrow(difficulty,Direction.UP,height)
-
-    # Initialize sprites
-    #arrowsprite = pygame.sprite.RenderPlain(uparrow)
-
-    # Create sequence of steps
-    seq = [
-        Arrow(difficulty, Direction.UP, 2, height),
-        Arrow(difficulty, Direction.LEFT, 3, height),
-        Arrow(difficulty, Direction.RIGHT, 4, height)
-    ]
-
-    # Blit everything to the screen
-    screen.blit(background, (0, 0))
+    #create the background, tile the bgd image
+    bgdtile = load_image('background.jpg')
+    background = pygame.Surface(SCREENRECT.size)
+    for x in range(0, SCREENRECT.width, bgdtile.get_width()):
+        background.blit(bgdtile, (x, 0))
+    screen.blit(background, (0,0))
     pygame.display.flip()
 
+    # Initialize Game Groups
+    arrows = pygame.sprite.Group()
+    all = pygame.sprite.RenderUpdates()
+
+    #assign default groups to each sprite class
+    Arrow.containers = all
+    
     # Initialize clock
     clock = pygame.time.Clock()
+
+    # Set the number of frames between new arrows
+    NEW_ARROW = 120          #FIX THIS TO INCORPORATE 60/(DIFF+1)
+    newarrow = NEW_ARROW
+
+    # Create a sequence of arrows, by direction
+    seq = (0, 1, 2, 3)
 
     i = 0
     # Event loop
     while 1:
-        # Make sure game doesn't run at more than 60 frames per second
-        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
-        if seq[i].get_time() > pygame.time.get_ticks():
-            arrowsprite = pygame.sprite.RenderPlain(seq[i])
+        # clear/erase the last drawn sprites
+        all.clear(screen, background)
 
-            screen.blit(background, seq[i].rect, seq[i].rect)
-            arrowsprite.update()
-            arrowsprite.draw(screen)
-            pygame.display.flip()
+        # update all the sprites
+        all.update()
 
+        # create new arrow
+        if newarrow:
+            newarrow = newarrow - 1
+        elif i < len(seq):
+            Arrow(seq[i])
+            newarrow = NEW_ARROW
+            i = i + 1
+        else:
+            Arrow(random.randint(0, 3))
+            newarrow = NEW_ARROW
+
+        #draw the scene
+        dirty = all.draw(screen)
+        pygame.display.update(dirty)
+
+        # Make sure game doesn't run at more than 60 frames per second
+        clock.tick(60)
+
+#call the "main" function if running this script
 if __name__ == '__main__': main()
